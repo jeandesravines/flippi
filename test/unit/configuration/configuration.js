96f48abfff2892f022412a0276199dbb46724bee
Jean Desravines <jean.desravines@gmail.com>
2016-09-26 20:31:55 +0200

    tests

--- test/unit/configuration/configuration.js
+++ test/unit/configuration/configuration.js
@@ -10,24 +10,37 @@ const environments = require('../../../lib/constant/environments');
 const devices = require('../../../lib/constant/devices');
 
 describe('Configuration', () => {
-    const filename = '../../../lib/configuration/configuration';
-    const configuration = require(filename);
-	const environment = Object.assign({}, process.env);
-    
 	describe('content', () => {
-        beforeEach('Delete require\' cache', () => {
-            delete require.cache[require.resolve(filename)];
-        });
-		
-		afterEach('Reset process.env', () => {
-			Object.assign(process.env, environment);
-            delete require.cache[require.resolve(filename)];
+		const filename = '../../../lib/configuration/configuration';
+		const configuration = require(filename);
+		const environment = Object.assign({}, process.env);
+
+		function clear() {
+			delete require.cache[require.resolve(filename)];
+		}
+
+		beforeEach('Delete require\'s cache', () => {
+			clear();
 		});
-        
+
+		afterEach('Reset process.env', () => {
+			clear();
+			Object.assign(process.env, environment);
+		});
+
 		it('should be the default configuration', () => {
 			expect(require(filename)).to.be.deep.equal(configuration);
 		});
-        
+
+		it('should be the default configuration without NODE_ENV', () => {
+			process.env.NODE_ENV = '';
+
+			const content = require(filename);
+			const environment = content.environment;
+
+			expect(environment).to.be.deep.equal(environments.production);
+		});
+
 		it('should be customized', () => {
 			process.env.FLIPPI_CHANNEL_MOTOR_1 = 'TEST_CHANNEL_MOTOR_1';
 			process.env.FLIPPI_DEVICE = devices.five;
@@ -36,9 +49,9 @@ describe('Configuration', () => {
 			process.env.NODE_ENV = environments.debug;
 
 			expect(require(filename)).to.be.deep.equal({
-                channels: {
-                    motor1: process.env.FLIPPI_CHANNEL_MOTOR_1
-                },
+				channels: {
+					motor1: process.env.FLIPPI_CHANNEL_MOTOR_1
+				},
 				debug: true,
 				device: process.env.FLIPPI_DEVICE,
 				environment: process.env.NODE_ENV,
