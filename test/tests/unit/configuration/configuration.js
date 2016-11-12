5e5ffc2fc2b9623427e7499678e6f1c9b7f18751
Jean Desravines <jean.desravines@gmail.com>
2016-11-12 11:26:13 +0100

    feat: Install process
    - install process
    - configuration (add bleno cone)
    - tests

--- test/tests/unit/configuration/configuration.js
+++ test/tests/unit/configuration/configuration.js
@@ -9,19 +9,21 @@ const {expect} = require('chai');
 const devices = require('../../../../lib/constant/devices');
 
 describe('Configuration', () => {
+  const filename = '../../../../lib/configuration/configuration';
+
   describe('content', () => {
     it('should be customized', () => {
-      const filename = '../../../../lib/configuration/configuration';
-
+      process.env.DEBUG = '*';
+      process.env.FLIPPI_ADVERTISING_INTERVAL = 200;
       process.env.FLIPPI_CHANNEL_0 = '10';
       process.env.FLIPPI_DEVICE = devices.gpio;
       process.env.FLIPPI_NAME = 'Hello';
       process.env.FLIPPI_PIN = 'TEST_PIN';
       process.env.FLIPPI_UUID_SERVICE = '110E8400-E29B-11D4-A716-446655440000';
       process.env.FLIPPI_UUID_SPEED = '110E8400-E29B-11D4-A716-446655440001';
-      process.env.DEBUG = '*';
 
       expect(require(filename)).to.be.deep.equal({
+        advertisingInterval: 200,
         channels: [
           10,
         ],
@@ -33,6 +35,18 @@ describe('Configuration', () => {
         device: devices.gpio,
         name: 'Hello',
         pin: 'TEST_PIN',
+      });
+    });
+
+    it('should change process.env', () => {
+      const configuration = require(filename);
+      const mapping = {
+        BLENO_ADVERTISING_INTERVAL: 'advertisingInterval',
+        BLENO_DEVICE_NAME: 'name',
+      };
+
+      Object.keys(mapping).forEach((key) => {
+        expect(process.env[key]).to.be.equal(configuration[mapping[key]].toString());
       });
     });
   });
